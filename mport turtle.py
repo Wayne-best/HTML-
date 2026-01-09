f73ad91bd0e6f40bb91b4aee28dcb5cfd7c14ea8
Wayne-best <waynem.m345@gmail.com>
2026-01-09 21:02:19 +0300

    Add files via upload

---
+++ mport turtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.Screen().bgcolor("Aqua")
+
+board=turtle.Turtle()
+board.left(120)
+board.right(100)
+board.left(120)
+board.right(100)
+board.penup()
+board.right(90)
+board.foward(50)
+turtle.done()
+
