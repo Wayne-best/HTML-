7c53a3d46eb9eb4fd21a972d92524d2cf48f2d95
Wayne-best <waynem.m345@gmail.com>
2026-02-06 20:57:48 +0300

    Add files via upload

---
+++ import sys.py
@@ -0,0 +1,14 @@
+import sys
+def intitial_phonebook():
+    rows,cols = int(input( "Please enter initial number of contacts: "))
+    phone_book = []
+    print(phone_book)
+    for i range in (rows):
+        print("\n enter contact %d details in the following orders (ONLY): "
+              % (i+1))
+        print("NOTE:* indicators mandatory fields")
+print(".............................................................................................")
+        temp = []
+        for j in range(cols)
+        if temp [j] == " " or temp [j] == " ":
+            
